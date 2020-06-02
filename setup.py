e79ef3170a7342240a0b42022d5c94b2fb814854
Tomasz Trębski <kornicameister@gmail.com>
2020-06-02 21:03:06 +0200

    pytest-mypy-plugin is not compatible at any point with 3.5

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ __author_email__ = 'kornicameister@gmail.com'
 __maintainer__ = __author__
 __url__ = 'https://github.com/kornicameister/loguru-mypy'
 
-if sys.version_info < (3, 5):
-    raise RuntimeError('loguru-mypy requires Python 3.5 or greater')
+if sys.version_info < (3, 6):
+    raise RuntimeError('loguru-mypy requires Python 3.6 or greater')
 
 setup(
     setup_requires='setupmeta',
-    python_requires='>=3.5.0',
+    python_requires='>=3.6.0',
     versioning='post',
 )
