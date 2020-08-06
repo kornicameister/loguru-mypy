a84b8172e88dfd5c1dec5aa2613f96fe6065ec05
Tomasz Trębski <kornicameister@gmail.com>
2020-08-06 23:40:44 +0200

    safe point, a lot of changes and something passes

--- loguru_mypy/__init__.py
+++ loguru_mypy/__init__.py
@@ -1,4 +1,5 @@
 import functools
+import re
 import string
 import typing as t
 
@@ -23,6 +24,8 @@ from mypy.types import (
 )
 import typing_extensions as te
 
+RECORD_ARG_REGEX = re.compile('record[["|\'](?P<arg>.*)["|\']]')  # type: te.Final
+
 ERROR_BAD_ARG: te.Final[ErrorCode] = ErrorCode(
     'logger-arg',
     'Positional argument of loguru handler is not valid for given message',
@@ -33,18 +36,44 @@ ERROR_BAD_KWARG: te.Final[ErrorCode] = ErrorCode(
     'Named argument of loguru handler is not valid for given message',
     'loguru',
 )
+ERROR_BAD_RECORD: te.Final[ErrorCode] = ErrorCode(
+    'logger-record',
+    'Invalid access to loguru record structure',
+    'loguru',
+)
 
 
 class Opts(t.NamedTuple):
     lazy: bool
+    record: bool
 
 
 DEFAULT_LAZY = False  # type: te.Final
-DEFAULT_OPTS = Opts(lazy=DEFAULT_LAZY)  # type: te.Final
+DEFAULT_RECORD = False  # type: te.Final
+DEFAULT_OPTS = Opts(
+    lazy=DEFAULT_LAZY,
+    record=DEFAULT_RECORD,
+)  # type: te.Final
 NAME_TO_BOOL = {
     'False': False,
     'True': True,
 }  # type: te.Final
+
+RECORD_ARGS = (
+    'elapsed',
+    'exception',
+    'extra',
+    'file',
+    'function',
+    'level',
+    'line',
+    'message',
+    'module',
+    'name',
+    'process',
+    'thread',
+    'time',
+)  # type: te.Final
 
 
 def _loguru_logger_call_handler(
@@ -77,13 +106,27 @@ def _loguru_logger_call_handler(
     log_msg_value: str = log_msg_expr.value
     log_msg_expected_args_count = 0
     log_msg_expected_kwargs = []
+    log_msg_record_references = []
+
     for res in string.Formatter().parse(log_msg_value):
-        if res[1] is None:
+        s_arg_name = res[1].strip() if res[1] is not None else None
+        if s_arg_name is None:
             continue
-        elif not res[1].strip():
+        elif not s_arg_name:
             log_msg_expected_args_count += 1
         else:
-            log_msg_expected_kwargs.append(res[1].strip())
+            maybe_record_match = RECORD_ARG_REGEX.search(s_arg_name)
+            if maybe_record_match:
+                log_msg_record_references.append(maybe_record_match.group('arg'))
+            else:
+                log_msg_expected_kwargs.append(s_arg_name)
+
+    _analyze_record_results(
+        log_msg_expr,
+        log_msg_record_references,
+        logger_opts,
+        ctx=ctx,
+    )
 
     if log_msg_expected_args_count > call_args_count:
         ctx.api.msg.fail(
@@ -152,6 +195,35 @@ def _loguru_logger_call_handler(
     return ctx.default_return_type
 
 
+def _analyze_record_results(
+    log_msg_expr: StrExpr,
+    log_msg_record_references: t.Sequence[str],
+    logger_opts: Opts,
+    *,
+    ctx: MethodContext,
+) -> None:
+    if logger_opts.record and not log_msg_record_references:
+        ctx.api.msg.note(
+            'Logger configured with record=True is not using record structure',
+            context=log_msg_expr,
+            code=ERROR_BAD_RECORD,
+        )
+    elif not logger_opts.record and log_msg_record_references:
+        ctx.api.msg.fail(
+            'Logger is accessing record structure without record=True',
+            context=log_msg_expr,
+            code=ERROR_BAD_RECORD,
+        )
+    elif logger_opts.record:
+        for record_attr in log_msg_record_references:
+            if record_attr not in RECORD_ARGS:
+                ctx.api.msg.fail(
+                    f'Logger record structure does not contain {record_attr} key',
+                    context=log_msg_expr,
+                    code=ERROR_BAD_RECORD,
+                )
+
+
 def _loguru_opt_call_handler(
     loggers: t.Dict[ProperType, Opts],
     ctx: MethodContext,
@@ -159,8 +231,18 @@ def _loguru_opt_call_handler(
     return_type = get_proper_type(ctx.default_return_type)
 
     lazy_expr = _get_opt_arg('lazy', ctx=ctx)
-    if isinstance(lazy_expr, NameExpr):
-        loggers[return_type] = Opts(lazy=NAME_TO_BOOL[lazy_expr.name])
+    record_expr = _get_opt_arg('record', ctx=ctx)
+
+    loggers[return_type] = Opts(
+        lazy=NAME_TO_BOOL[lazy_expr.name] if isinstance(
+            lazy_expr,
+            NameExpr,
+        ) else False,
+        record=NAME_TO_BOOL[record_expr.name] if isinstance(
+            record_expr,
+            NameExpr,
+        ) else False,
+    )
 
     return return_type
 
