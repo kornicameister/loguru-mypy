c0aa3da7967f28360897713aa293080eb2184034
Tomasz Trębski <kornicameister@gmail.com>
2020-11-08 21:17:22 +0100

    Pin mypy version on lower boundary

--- loguru_mypy/__init__.py
+++ loguru_mypy/__init__.py
@@ -212,5 +212,13 @@ class LoguruPlugin(Plugin):
         return super().get_method_hook(fullname)
 
 
+class UnsupportedMypyVersion(RuntimeError):
+    def __init__(self, version: str) -> None:
+        super().__init__(f'Mypy {version} is not supported')
+
+
 def plugin(version: str) -> t.Type[LoguruPlugin]:
+    _, minor = tuple(map(int, version.split('.')))
+    if minor < 770:
+        raise UnsupportedMypyVersion(version)
     return LoguruPlugin
